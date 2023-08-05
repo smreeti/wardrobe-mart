cb6ed1c002fc6b0f6e428ed33ef3db6ab5bd8c43
nikesh <nikeshtiwari3230@gmail.com>
2023-08-05 19:14:04 -0400

    Login and Signup page Created

--- src/components/LoginComponent.jsx
+++ src/components/LoginComponent.jsx
@@ -1,3 +1,55 @@
-export default function LoginComponent(){
-    
-}
+import React, { useState } from 'react';
+
+const LoginComponent = () => {
+  const [loginFormData, setLoginFormData] = useState({
+    email: '',
+    password: '',
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setLoginFormData({
+      ...loginFormData,
+      [name]: value,
+    });
+  };
+
+  const handleLoginSubmit = (e) => {
+    e.preventDefault();
+
+    console.log('Logging in...');
+    console.log(loginFormData);
+
+  };
+
+  return (
+    <div className="container-login">
+      <div className="form-wrapper">
+        <h1>Login</h1>
+        <form onSubmit={handleLoginSubmit}>
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={loginFormData.email}
+            onChange={handleInputChange}
+            required
+          />
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={loginFormData.password}
+            onChange={handleInputChange}
+            required
+          />
+          <button type="submit">Login</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default LoginComponent;
--- src/components/SignupComponent.jsx
+++ src/components/SignupComponent.jsx
@@ -1,5 +1,65 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-export default function SignupComponent(){
-    
-}
+const SignupComponent = () => {
+  const [signupFormData, setSignupFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setSignupFormData({
+      ...signupFormData,
+      [name]: value,
+    });
+  };
+
+  const handleSignupSubmit = (e) => {
+    e.preventDefault();
+
+    console.log('Signing up...');
+    console.log(signupFormData);
+
+  };
+
+  return (
+    <div className="container-signup">
+      <div className="form-wrapper">
+        <h1>Signup</h1>
+        <form onSubmit={handleSignupSubmit}>
+          <label htmlFor="name">Name:</label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={signupFormData.name}
+            onChange={handleInputChange}
+            required
+          />
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={signupFormData.email}
+            onChange={handleInputChange}
+            required
+          />
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={signupFormData.password}
+            onChange={handleInputChange}
+            required
+          />
+          <button type="submit">Signup</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default SignupComponent;
